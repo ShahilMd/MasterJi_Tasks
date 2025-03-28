d6aa5ef71b716c9ceace8e0a3fa19d65e454f44f
Md Shahil <shahil.connect@gmail.com>
2025-03-28 17:07:06 +0000

    update code try to do by self

--- MarkdownConverter/index.js
+++ MarkdownConverter/index.js
@@ -1,53 +1,48 @@
+const inputText = document.getElementById('input-text');
+const previewArea = document.getElementById('preview-area')
+const copyBtn = document.querySelector(".btn-copy");
+const clearBtn = document.querySelector(".btn-clear");
 
-const previewArea = document.getElementById("preview-area");
-const inputText = document.getElementById("input-text");
-const copyBtn = document.getElementById("copy-btn");
-const clearBtn = document.getElementById("clear-btn");
+function initialText(){
+  //initial welcome message
+ const iniText=`
+  # Welcome to Modern Markdown
+  
+  ## Explore Powerful Formatting
+    
+  This is a **modern** and *stylish* Markdown converter.
+    
+  ### Key Features
+  - Real-time preview
+  - Beautiful typography
+  - Responsive design
+    
+   [Learn More](https://example.com)`;
+   //load initial value
+    inputText.innerHTML=iniText
 
-// Example placeholder text
-inputText.value = `# Welcome to Modern Markdown
-
-## Explore Powerful Formatting
-
-This is a **modern** and *stylish* Markdown converter.
-
-### Key Features
-- Real-time preview
-- Beautiful typography
-- Responsive design
-
-[Learn More](https://example.com)`;
-function clearInitial(){
-  inputText.value='';
 }
-function updatePreview() {
-  previewArea.innerHTML=inputText.value;
-  inputText.addEventListener('click',clearInitial)
-  previewArea.innerHTML = marked.parse(inputText.value);
-};
-
-
-        // Copy Markdown
-        copyBtn.addEventListener('click', function() {
-          navigator.clipboard.writeText(previewArea.textContent).then(() => {
-              this.textContent = 'Copied!';
-              this.style.background = 'linear-gradient(135deg, #10b981, #059669)';
-              
-              setTimeout(() => {
-                  this.textContent = 'Copy Markdown';
-                  this.style.background = 'linear-gradient(135deg, #22d3ee, #06b6d4)';
-              }, 2000);
-          }).catch(err => {
-              console.error('Copy failed', err);
-          });
-      });
-
-      // Clear input and preview
-      clearBtn.addEventListener('click', function() {
-        inputText.value = '';
-        previewArea.innerHTML = '';
-    });
+//clear input feild for writing
+function clearInput(){
+  inputText.innerHTML=''
+  
+}
+function updateMd(){
+  let text = marked.parse(inputText.value)
+  previewArea.innerHTML=text
+}
+// copy button funcnality 
+function copyText(){
+  setTimeout(()=>{
+   copyBtn.innerHTML='Copied';
+  })
+ 
 
 
 
-setInterval(updatePreview, 100);
+}
+
+inputText.addEventListener('click',clearInput)
+initialText()
+copyBtn.addEventListener('click',copyText)
+setInterval(updateMd, 100);
